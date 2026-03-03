a13b311ee2fe4a7d52043d18fda8aeebca29ff83
Priyankan <priyankanvettivel@gmail.com>
2026-03-03 23:25:34 +0530

    Clean tracked files

--- target/classes/db/migration/V2__insert_test_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (name, account_number, pin_hash, balance)
-VALUES ('Priyankan', '1234567890', '1234', 10000.00);
-
-INSERT INTO admins (name, username, password_hash, role)
-VALUES ('SuperAdmin', 'admin', 'admin123', 'SUPER_ADMIN');
